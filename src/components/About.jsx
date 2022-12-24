e3bd16cee60c55d8649316366b509e133860c1f2
matiaschanquia <matiaschanquianahuel@gmail.com>
2022-12-24 17:33:04 -0300

    about commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ export default function About(props) {
             </figure>
             <div className={style.sobre}>
                 <h2>Sobre la web:</h2>
-                <p>Esta aplicación web permite buscar gifs por texto y se mostrarán 10 gifs en pantalla. Añádí un loading al buscar los gifs y también para la carga de los propios gifs.</p>
+                <p>Esta aplicación web permite buscar gifs por texto y se mostrarán 10 gifs en pantalla. Haciendo click en el nombre del gif se puede ver el gif en mayor tamaño. Añádí un loading al buscar los gifs y también para la carga de los propios gifs.</p>
                 <h2>Sobre mi:</h2>
                 <p>Soy Matias Chanquia de Córdoba, Argentina, tengo 21 años y soy desarrollador frontend. Amante de la programación desde el 2020 que empecé con Python.</p>
                 <p>Mi email es: <a href="mailto:matiaschanquianahuel@gmail.com">matiaschanquianahuel@gmail.com</a></p>
